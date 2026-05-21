4cf3df75fdf1417ff8ad09686c916ad1d5edaeda
app <app@Mirza-Rayan.local>
2026-05-21 14:34:59 +0500

    deleteReview controller created successfully

--- src/controllers/review.controllers.js
+++ src/controllers/review.controllers.js
@@ -206,9 +206,42 @@ const getSingleReview = async (req, res) => {
 }
 
 
+const deleteReview = async (req, res) => {
+    try {
+        const singleReview = await Review.findById(req.params.id)
+
+        if(!singleReview) {
+             return res.status(404).json({
+                message: 'review not found'
+             })
+        }
+
+        if(singleReview.userId.toString() !== req.user._id.toString()) {
+            return res.status(403).json({
+                message: 'You cannot delete reviews of other users'
+            })
+        }
+
+        await Review.findByIdAndDelete(req.params.id)
+
+
+        return res.status(200).json({
+            message: 'review deleted successfully',
+        })
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: 'Server Error while deleting review'
+        })
+    }
+}
+
 
 export {
     reviewCode,
     getAllReviews,
-    getSingleReview
+    getSingleReview,
+    deleteReview
 }
--- src/routes/review.routes.js
+++ src/routes/review.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/Auth.middleware.js";
-import { getAllReviews, getSingleReview, reviewCode } from "../controllers/review.controllers.js";
+import { deleteReview, getAllReviews, getSingleReview, reviewCode } from "../controllers/review.controllers.js";
 
 const router = Router();
 
@@ -11,6 +11,7 @@ const router = Router();
 router.route('/review-code').post(verifyJWT, reviewCode)
 router.route('/get-reviews').get(verifyJWT, getAllReviews)
 router.route('/get-review/:id').get(verifyJWT, getSingleReview)
+router.route('/delete-review/:id').delete(verifyJWT, deleteReview)
 
 
 
